994fb032b828165afa3dbc3178b9a0180df08e78
Enes YALÇIN <eenes.ylcn@gmail.com>
2025-03-17 01:46:14 +0300

    Interfaces were created for the Repository Design Pattern.

---
+++ MatchwiseServer/Core/MatchwiseServer.Application/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatchwiseServer.Application.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        DbSet<T> Table {  get; }
+    }
+}

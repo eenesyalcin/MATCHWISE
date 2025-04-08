b30347d0d9856accda45609009136bbe70e6f514
Enes YALÇIN <eenes.ylcn@gmail.com>
2025-04-08 17:56:32 +0300

    Corporate login page has been designed.

--- MatchwiseClient/src/app/components/auth/corporate/corporate-login/corporate-login.component.ts
+++ MatchwiseClient/src/app/components/auth/corporate/corporate-login/corporate-login.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-corporate-login',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './corporate-login.component.html',
   styleUrl: './corporate-login.component.scss'
 })

6b2156a083c1fd459b755b5203c650a7d32017c3
Enes YALÇIN <eenes.ylcn@gmail.com>
2025-04-09 12:48:58 +0300

    Corporate registration page has been designed.

--- MatchwiseClient/src/app/components/auth/corporate/corporate-register/corporate-register.component.ts
+++ MatchwiseClient/src/app/components/auth/corporate/corporate-register/corporate-register.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-corporate-register',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './corporate-register.component.html',
   styleUrl: './corporate-register.component.scss'
 })

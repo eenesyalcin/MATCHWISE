6e9c85f77695063f4dbc06728420a68b8474bf60
Enes YALÇIN <eenes.ylcn@gmail.com>
2025-04-09 14:29:18 +0300

    A login button has been added to the navigation bar on the home page.

--- MatchwiseClient/src/app/components/home/layout/navbar/navbar.component.ts
+++ MatchwiseClient/src/app/components/home/layout/navbar/navbar.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
+  loginMenuOpen = false;
+
+  toggleLoginMenu() {
+    this.loginMenuOpen = !this.loginMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.btn-group')) {
+      this.loginMenuOpen = false;
+    }
+  }
 
 }

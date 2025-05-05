5bedbd199744e269cfd32242f5e47cb581579efa
Enes YALÇIN <eenes.ylcn@gmail.com>
2025-05-05 22:25:20 +0300

    Token creation process for the candidate in the backend has been completed.

--- MatchwiseServer/Core/MatchwiseServer.Application/Features/Commands/Candidate/LoginCandidate/LoginCandidateCommandResponse.cs
+++ MatchwiseServer/Core/MatchwiseServer.Application/Features/Commands/Candidate/LoginCandidate/LoginCandidateCommandResponse.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MatchwiseServer.Application.DTOs;
 
 namespace MatchwiseServer.Application.Features.Commands.Candidate.LoginCandidate
 {
     public class LoginCandidateCommandResponse
     {
+    }
+
+    public class LoginCandidateSuccessCommandResponse : LoginCandidateCommandResponse
+    {
+        public Token Token { get; set; }
+    }
+
+    public class LoginCandidateErrorCommandResponse : LoginCandidateCommandResponse
+    {
+        public string Message { get; set; }
     }
 }

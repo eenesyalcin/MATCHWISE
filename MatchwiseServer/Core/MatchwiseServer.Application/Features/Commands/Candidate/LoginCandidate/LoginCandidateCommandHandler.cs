f1011acdfb1213a0d44a8a225e4b5160dc767f6e
Enes YALÇIN <eenes.ylcn@gmail.com>
2025-05-05 14:04:23 +0300

    Authentication operations were performed for individual login on the backend.

--- MatchwiseServer/Core/MatchwiseServer.Application/Features/Commands/Candidate/LoginCandidate/LoginCandidateCommandHandler.cs
+++ MatchwiseServer/Core/MatchwiseServer.Application/Features/Commands/Candidate/LoginCandidate/LoginCandidateCommandHandler.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MatchwiseServer.Application.Exceptions;
+using MatchwiseServer.Domain.Entities.Identity;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 
 namespace MatchwiseServer.Application.Features.Commands.Candidate.LoginCandidate
 {
     public class LoginCandidateCommandHandler : IRequestHandler<LoginCandidateCommandRequest, LoginCandidateCommandResponse>
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginCandidateCommandHandler(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public async Task<LoginCandidateCommandResponse> Handle(LoginCandidateCommandRequest request, CancellationToken cancellationToken)
         {
-            return null;
+            AppUser user = await _userManager.FindByEmailAsync(request.Email);
+            if (user == null)
+            {
+                throw new NotFoundUserException("Email veya şifre hatalı!");
+            }
+
+            SignInResult result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+            if (result.Succeeded) // True dönerse Authentication başarılı!
+            {
+                // Yetkilendirme işlemleri...
+            }
+
+            return new();
         }
     }
 }

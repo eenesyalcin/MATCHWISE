970f94f7089872b968cc810afd1b2f5244a05424
Enes YALÇIN <eenes.ylcn@gmail.com>
2025-05-06 17:09:15 +0300

    Connection string configuration was edited so that the project can run on another machine.

--- MatchwiseServer/Infrastructure/MatchwiseServer.Persistence/ServiceRegistration.cs
+++ MatchwiseServer/Infrastructure/MatchwiseServer.Persistence/ServiceRegistration.cs
@@ -14,7 +14,24 @@ namespace MatchwiseServer.Persistence
     {
         public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
         {
-            string? connectionString = Environment.GetEnvironmentVariable("MATCHWISE_CONNECTION_STRING");
+            string? environmentConnectionString = Environment.GetEnvironmentVariable("MATCHWISE_CONNECTION_STRING");
+            string connectionString;
+            if (!string.IsNullOrEmpty(environmentConnectionString))
+            {
+                connectionString = environmentConnectionString;
+                Console.WriteLine("⮕ Bağlantı dizesi ortam değişkeninden alındı: MATCHWISE_CONNECTION_STRING");
+            }
+            else
+            {
+                var defaultConnectionString = configuration.GetConnectionString("DefaultConnection");
+                if (string.IsNullOrEmpty(defaultConnectionString))
+                {
+                    throw new InvalidOperationException("Configuration içinde 'ConnectionStrings:DefaultConnection' bulunamadı.");
+                }
+                connectionString = defaultConnectionString;
+                Console.WriteLine("⮕ Bağlantı dizesi appsettings.json’dan alındı: DefaultConnection");
+            }
+
 
             services.AddDbContext<MatchwiseServerDbContext>(options =>
                 options.UseSqlServer(connectionString));

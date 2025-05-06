8303a36b4129b0f505368177f5ab48f36480bda4
Enes YALÇIN <eenes.ylcn@gmail.com>
2025-05-06 12:01:19 +0300

    Interview page designed.

--- MatchwiseClient/src/app/components/interview/interview.component.ts
+++ MatchwiseClient/src/app/components/interview/interview.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-interview',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './interview.component.html',
   styleUrl: './interview.component.scss'
 })
 export class InterviewComponent {
+
+  loginMenuOpen = false;
+
+  toggleLoginMenu() {
+    this.loginMenuOpen = !this.loginMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.btn-group')) {
+      this.loginMenuOpen = false;
+    }
+  }
 
 }
